a7e9a27390af1c0df4f53f9237857fe55ddc358d
404DEVR <rdev36437@gmail.com>
2024-10-21 18:46:08 +0530

    few

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -21,9 +21,55 @@ const RightSidebar = () => {
 
   return (
     <div className="relative flex justify-end top-5 gap-4 max-md:hidden ">
-      <Button onClick={() => setIsPressed(!isPressed)} className="z-10">
-        Find Podcast creaters
-      </Button>
+      <div className="relative">
+        <Button onClick={() => setIsPressed(!isPressed)} className="z-10">
+          Find Podcast creaters
+        </Button>
+        {isPressed && (
+          <section
+            className={`right_sidebar text-white-1 h-[calc(100vh-100px)] top-0 right-44`}
+          >
+            <div>
+              <section>
+                <Header headerTitle=" Fans Like You" />
+                <Carousel fansLikeDetail={topPodcasters!} />
+              </section>
+              <section className="flex flex-col gap-8 pt-6">
+                <Header
+                  headerTitle="Top Podcasters"
+                  headerClassname=""
+                ></Header>
+                <div className="flex flex-col gap-6 mb-4">
+                  {topPodcasters?.slice(0, 4).map((item) => (
+                    <div
+                      key={item._id}
+                      className="flex cursor-pointer justify-between"
+                      onClick={() => router.push(`/profile/${item.clerkId}`)}
+                    >
+                      <figure className="flex items-center gap-2">
+                        <Image
+                          src={item.imageUrl}
+                          alt={item.name}
+                          className="aspect-square rounded-lg"
+                          width={44}
+                          height={44}
+                        />
+                        <h2 className="text-14 font-semibold">{item.name}</h2>
+                      </figure>
+                      <div className="flex items-center">
+                        <p className="text-12 font-normal">
+                          {item.totalPodcasts}{" "}
+                          {item.totalPodcasts === 1 ? "podcast" : "podcasts"}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </section>
+        )}
+      </div>
       <SignedIn>
         <Button className="right-5 top-5 rounded-lg gap-2">
           <Link href={`/profile/${user?.id}`} className="flex gap-2">
@@ -36,47 +82,6 @@ const RightSidebar = () => {
           </Link>
         </Button>
       </SignedIn>
-      {isPressed && (
-        <section
-          className={`right_sidebar text-white-1 h-[calc(100vh-100px)] top-0 right-80`}
-        >
-          <div>
-            <section>
-              <Header headerTitle=" Fans Like You" />
-              <Carousel fansLikeDetail={topPodcasters!} />
-            </section>
-            <section className="flex flex-col gap-8 pt-6">
-              <Header headerTitle="Top Podcasters" headerClassname=""></Header>
-              <div className="flex flex-col gap-6 mb-4">
-                {topPodcasters?.slice(0, 4).map((item) => (
-                  <div
-                    key={item._id}
-                    className="flex cursor-pointer justify-between"
-                    onClick={() => router.push(`/profile/${item.clerkId}`)}
-                  >
-                    <figure className="flex items-center gap-2">
-                      <Image
-                        src={item.imageUrl}
-                        alt={item.name}
-                        className="aspect-square rounded-lg"
-                        width={44}
-                        height={44}
-                      />
-                      <h2 className="text-14 font-semibold">{item.name}</h2>
-                    </figure>
-                    <div className="flex items-center">
-                      <p className="text-12 font-normal">
-                        {item.totalPodcasts}{" "}
-                        {item.totalPodcasts === 1 ? "podcast" : "podcasts"}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </section>
-          </div>
-        </section>
-      )}
     </div>
   );
 }
